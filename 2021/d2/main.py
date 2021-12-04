d4e82395d2837f883b8d3dfcf8909478445f7b30
Saobangmath <anhtai2672000@gmail.com>
2021-12-04 17:07:16 +0800

    d2-2021

---
+++ 2021/d2/main.py
@@ -0,0 +1,45 @@
+from functools import reduce
+
+
+def read() -> list[tuple[int]]:
+    res = []
+    with open("input.txt") as f:
+        for _ in f:
+            op, change = _.split(" ")
+            change = int(change)
+            if op == "forward":
+                res.append((0, change))
+            if op == "down":
+                res.append((1, change))
+            if op == "up":
+                res.append((1, -change))
+        f.close()
+    return res
+
+
+def part1(changes):
+    dims = [0, 0]
+    for change in changes:
+        dims[change[0]] += change[1]
+    print(reduce(lambda u, v: abs(u) * abs(v), dims))
+
+
+def part2(changes):
+    dims = [0, 0]
+    aim = 0
+    for change in changes:
+        if change[0] == 1:
+            aim += change[1]
+        else:
+            dims[0] += change[1]
+            dims[1] += change[1] * aim
+    print(reduce(lambda u, v: abs(u) * abs(v), dims))
+
+
+def main():
+    changes = read()
+    part1(changes)
+    part2(changes)
+
+
+main()
